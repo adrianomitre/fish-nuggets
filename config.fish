1663b75470130e7784e712ae87d0f3f6ba5fc012
Adriano Mitre <adriano.mitre@gmail.com>
2011-01-03 20:43:18 -0200

    removed unecessary stuff which was already commented

--- config.fish
+++ config.fish
@@ -2,8 +2,6 @@ set -xg RUBYOPT $RUBYOPT rubygems
 set -xg JAVA_HOME /usr/java/jdk1.6.0_17
 set -xg PATH $PATH /usr/java/jdk1.6.0_17/bin
 
-# set -xg CVSROOT amitre@turing.liv.ic.unicamp.br:/LIV/axf/cvsroot
-# set -xg CVS_RSH ssh
-
-# set -xg SDL_audiodriver alsa
+alias less="less -R"
+alias lock='gnome-screensaver-command --lock'
 
